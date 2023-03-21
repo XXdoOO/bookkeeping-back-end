7ec332db3b411bb2431bca2ce6d701426982cf47
谢翔 <1972524359@qq.com>
2023-03-21 15:52:40 +0800

    记录

--- src/main/java/com/xx/config/IntercaptorConfig.java
+++ src/main/java/com/xx/config/IntercaptorConfig.java
@@ -13,6 +13,6 @@ public class IntercaptorConfig implements WebMvcConfigurer {
                 //拦截的路径
                 .addPathPatterns("/**")
                 //排除登录接口
-                .excludePathPatterns("/login");
+                .excludePathPatterns("/login").addPathPatterns("/notLogin");
     }
 }
